865e4cf81ce6ffb2121b28cbfa89f770a7ef16cb
pj6444 <pj6444@gmail.com>
2017-11-19 13:52:49 -0600

    Set up login/registration with Firebase

---
+++ app/src/main/java/com/patrickfeltes/graphingprogram/CreateAccountActivity.java
@@ -0,0 +1,56 @@
+package com.patrickfeltes.graphingprogram;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class CreateAccountActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private EditText emailField;
+    private EditText passwordField;
+    private Button createAccountButton;
+
+    private FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        emailField = (EditText) findViewById(R.id.et_email);
+        passwordField = (EditText) findViewById(R.id.et_password);
+        createAccountButton = (Button) findViewById(R.id.b_create_account);
+
+        createAccountButton.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        String email = emailField.getText().toString();
+        String password = passwordField.getText().toString();
+
+        firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(CreateAccountActivity.this, "Registration Successful", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(CreateAccountActivity.this, "Invalid username/password combo", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/patrickfeltes/graphingprogram/LoginActivity.java
@@ -0,0 +1,64 @@
+package com.patrickfeltes.graphingprogram;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private FirebaseAuth firebaseAuth;
+
+    private EditText emailField;
+    private EditText passwordField;
+    private Button loginButton;
+    private Button registerButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        emailField = (EditText) findViewById(R.id.et_email);
+        passwordField = (EditText) findViewById(R.id.et_password);
+        loginButton = (Button) findViewById(R.id.b_login);
+        registerButton = (Button) findViewById(R.id.b_register);
+
+        loginButton.setOnClickListener(this);
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(view.getContext(), CreateAccountActivity.class));
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View view) {
+        String email = emailField.getText().toString();
+        String password = passwordField.getText().toString();
+
+        firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(LoginActivity.this, firebaseAuth.getCurrentUser().getEmail(), Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(LoginActivity.this, "Invalid login credentials", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
