4a69ae81c77da3c11e02b5da71f0576184972001
pj6444 <pj6444@gmail.com>
2017-12-06 15:21:04 -0600

    Add graph interface, may change later for more flexibility

--- app/src/main/java/com/patrickfeltes/graphingprogram/graphs/GraphingFragment.java
+++ app/src/main/java/com/patrickfeltes/graphingprogram/graphs/GraphingFragment.java
@@ -1,6 +1,49 @@
 package com.patrickfeltes.graphingprogram.graphs;
 
 import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+import com.patrickfeltes.graphingprogram.R;
 
 public class GraphingFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.graphing_layout, container, false);
+        
+        // code from:
+        // http://www.android-graphview.org/zooming-and-scrolling/
+        GraphView graph = (GraphView) view.findViewById(R.id.graph);
+        // first series is a line
+        DataPoint[] points = new DataPoint[100];
+        for (int i = 0; i < points.length; i++) {
+            points[i] = new DataPoint(i, Math.sin(i*0.5) * 20*(Math.random()*10+1));
+        }
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(points);
+
+        // set manual X bounds
+        graph.getViewport().setYAxisBoundsManual(true);
+        graph.getViewport().setMinY(-150);
+        graph.getViewport().setMaxY(150);
+
+        graph.getViewport().setXAxisBoundsManual(true);
+        graph.getViewport().setMinX(4);
+        graph.getViewport().setMaxX(80);
+
+        // enable scaling and scrolling
+        graph.getViewport().setScalable(true);
+        graph.getViewport().setScalableY(true);
+
+        graph.addSeries(series);
+
+        return view;
+    }
 }
